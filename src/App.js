4dd5e502c18f42fa9c206603e0b6a617dc0950c1
Hasul79 <hatuk2@mail.ru>
2022-12-22 23:03:52 +0400

    add image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { Button, Divider, Form , Input, message, Typography } from 'antd';
+import {GoogleOutlined,FacebookFilled,TwitterOutlined  } from "@ant-design/icons";
 import './App.css';
 
+
 function App() {
+
+  const login = () => {
+    message.success('Login Successful!');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appBg">
+      <Form className='loginForm' onFinish={login}>
+        <Typography.Title> Welcome Back!</Typography.Title>
+        <Form.Item 
+        rules={[
+          {
+          required: true,
+          type:'email',
+          message:'Please enter valid email' ,
+        },
+      ]}
+        label='Email' name={'myEmail'}>
+          <Input placeholder="Enter your email" />
+        </Form.Item>
+        <Form.Item 
+        rules={[
+          {
+          required: true,
+          message:'Please enter your password' ,
+        },
+      ]}
+        label='Password' name={'myPassword'}>
+          <Input.Password placeholder="Enter your password" />
+        </Form.Item>
+        <Button type='primary' htmlType='submit' block>
+          Login
+        </Button>
+        <Divider style={{borderColor:"black"}}>or Login with</Divider>
+        <div className='socialLogin'>
+          <GoogleOutlined className='socialIcon' onClick={login} style={{color:"red"}}/>
+          <FacebookFilled className='socialIcon' onClick={login} style={{color:"blue"}}/>
+          <TwitterOutlined className='socialIcon' onClick={login} style={{color:"cyan"}}/>
+        </div>
+      </Form>     
     </div>
   );
 }
